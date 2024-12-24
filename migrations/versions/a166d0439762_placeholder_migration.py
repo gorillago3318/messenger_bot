80a67057e8d3ad21ec387589f3e7cd98ff55b471
gorillago3318 <makwaikit@gmail.com>
2024-12-24 23:39:10 +0800

    Added migration version files

---
+++ migrations/versions/a166d0439762_placeholder_migration.py
@@ -0,0 +1,22 @@
+"""placeholder migration
+
+Revision ID: a166d0439762
+Revises: 
+Create Date: 2024-12-24 23:33:22.606781
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a166d0439762'
+down_revision = None  # Adjust if dependencies exist
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
